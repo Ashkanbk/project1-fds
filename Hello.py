47aa7b8f2ed8e5d03052654fd8334b07eb013b96
Ashkanbk <145492288+Ashkanbk@users.noreply.github.com>
2023-10-08 23:39:27 -0400

    added

--- Hello.py
+++ Hello.py
@@ -1,33 +1,58 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
+import numpy as np
+import seaborn as sns
 
 st.image("header.JPG", use_column_width="always")
+
+ra=pd.read_csv('fifa.csv')
+rs=pd.read_csv('results.csv')
+gs=pd.read_csv('goalscorers.csv')
+
+
+### st.title('1- Raw data')
+
+_ ='''
+dbs = st.radio(
+    "What's your favorite movie genre",
+    ["ra", "rs", "gs"],
+    horizontal=True
+)
+if dbs == "ra":
+    st.dataframe(ra)
+elif dbs == "rs":
+    st.dataframe(rs)
+elif dbs == "gs":
+    st.dataframe(gs)
+st.dataframe(gs)
+'''
+
 
 st.title('History of matches')
 
 col1, col2 = st.columns(2)
 
 with col1:
-    Home = st.selectbox(
-        'Home Team:',
+    first = st.selectbox(
+        'First Team:',
         ('England', 'France', 'Italy')
     )
 
 
 with col2:
-    Away = st.selectbox(
-        'Away Team:',
+    second = st.selectbox(
+        'Second Team:',
         ('France', 'England', 'Italy')
     )
 
-ra=pd.read_csv('fifa.csv')
-rs=pd.read_csv('results.csv')
+
+newra=rs[((rs['home_team'] == first) & (rs['away_team'] == second))|((rs['home_team'] == second) & (rs['away_team'] == first))]
+
+newrash=newra[['date','home_team','home_score','away_score','away_team','tournament','city']]
 
 
-newra=rs[(rs['home_team'] == 'England') & (rs['away_team'] == 'France')]
+
 def calculate_result(row):
     if row['home_score'] > row['away_score']:
         return 'Win'
@@ -36,27 +61,103 @@ def calculate_result(row):
     else:
         return 'Draw'
 
-# Apply the function to create a new column 'result'
 newra['result'] = newra.apply(calculate_result, axis=1)
 
-win_counts = newra['result'].value_counts()
+T1=newra[(newra['home_team']==first) & (newra['neutral']==False)]
+T1['result'].value_counts()
+T2=newra[(newra['away_team']==first) & (newra['neutral']==False)]
+replacements = {'Lose': 'Win', 'Win': 'Lose'}
+T2['result'] = T2['result'].replace(replacements)
+T2['result'].value_counts()
+T3=newra[newra['neutral']==True]
+T3['result'].value_counts()
+df_T1 = pd.DataFrame(T1['result'].value_counts())
+df_T2 = pd.DataFrame(T2['result'].value_counts())
+df_T3 = pd.DataFrame(T3['result'].value_counts())
+combined_df = pd.concat([df_T1, df_T2, df_T3], axis=1)
+combined_df.columns = ['Home', 'Away', 'Neutral']
+combined_df['Total'] = combined_df['Home'] + combined_df['Away'] + combined_df['Away']
 
-font = {'family': 'serif', 'color': 'darkred', 'weight': 'normal', 'size': 16}
-
-# Create a bar plot
-fig, ax = plt.subplots()
-ax.bar(win_counts.index, win_counts)
-ax.set_title('Winning Statistics', fontdict=font)
-
-    
-# Show the plot
-st.pyplot(fig)
 
 
+# Radio button for selecting number or percent
+display_type = st.radio("Display Type", ["Number", "Percent"], horizontal=True)
 
-newra=rs[(rs['home_team'] == Home) & (rs['away_team'] == Away)]
+# Checkbox for selecting columns
+st.write("Select Columns")
 
-st.dataframe(newra)
+col1, col2, col3 ,col4  = st.columns(4)
+with col1:
+    show_home = st.checkbox("Home")
+with col2:
+    show_away = st.checkbox("Away")
+with col3:
+    show_neutral = st.checkbox("Neutral")
+with col4:
+    show_total = st.checkbox("Total")
+
+
+
+# Define custom colors for columns
+column_colors = {
+    'Home': 'slateblue',
+    'Away': 'chocolate',
+    'Neutral': 'gold',
+    'Total': 'gray'
+}
+
+# Filter the columns to display
+selected_columns = []
+selected_colors = []
+if show_home:
+    selected_columns.append('Home')
+    selected_colors.append(column_colors['Home'])
+if show_away:
+    selected_columns.append('Away')
+    selected_colors.append(column_colors['Away'])
+if show_neutral:
+    selected_columns.append('Neutral')
+    selected_colors.append(column_colors['Neutral'])
+if show_total:
+    selected_columns.append('Total')
+    selected_colors.append(column_colors['Total'])
+
+# Create a subset DataFrame based on the selected columns
+subset_df = combined_df[selected_columns]
+
+# Calculate percentages if needed
+if display_type == "Percent":
+    subset_df = subset_df.div(subset_df.sum(axis=0), axis=1) * 100
+
+# Create a grouped bar plot with custom colors
+plt.figure(figsize=(10, 6))
+categories = subset_df.index
+columns = subset_df.columns
+bar_width = 0.2
+index = range(len(categories))
+
+for i, (col, color) in enumerate(zip(columns, selected_colors)):
+    plt.bar([x + i * bar_width for x in index], subset_df[col], bar_width, label=col, color=color, edgecolor='black', linewidth=1)
+
+ax = plt.gca()
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+
+plt.ylabel(display_type)
+plt.title(f'Stats of {first} in head to head matches with {second}')
+plt.xticks([x + bar_width for x in index], categories)
+plt.legend()
+
+# Display the plot in Streamlit
+st.pyplot(plt)
+
+
+st.dataframe(newrash,use_container_width=True)
+
+
+
+
 
 
 dfEn=ra[ra['country_full'] == 'England']
@@ -124,3 +225,31 @@ folium_map_html = m._repr_html_()
 # Display the map in the Streamlit app
 st.write("City Locations on World Map")
 st.components.v1.html(folium_map_html, width=800, height=600)
+
+
+
+# Sample data
+data = {
+    'Group': ['A', 'A', 'B', 'B', 'C', 'C'],
+    'Category': ['Category 1', 'Category 2', 'Category 1', 'Category 2', 'Category 1', 'Category 2'],
+    'Value': [10, 15, 12, 18, 8, 10]
+}
+
+df = pd.DataFrame(data)
+
+# Streamlit app
+st.title('Grouped Barplots with Checkboxes')
+
+# Checkboxes for group selection
+selected_groups = st.multiselect('Select Groups', df['Group'].unique())
+
+# Filter the DataFrame based on selected groups
+filtered_df = df[df['Group'].isin(selected_groups)]
+
+# Create a grouped barplot
+if not filtered_df.empty:
+    fig, ax = plt.subplots()
+    sns.barplot(data=filtered_df, x='Category', y='Value', hue='Group', ax=ax)
+    st.pyplot(fig)
+else:
+    st.warning('No data to display for the selected groups.')
